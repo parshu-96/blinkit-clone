1c3f699fc9cb9e08aebcab41e8776ba52028f451
parshu-96 <parshuvmahindrakar@gmail.com>
2024-11-22 22:33:58 +0530

    Added All the DB models
    Added All the DB models

---
+++ server/models/order.model.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+    },
+    orderId: {
+      type: String,
+      required: [true, "Provided order ID"],
+      unique: true,
+    },
+    productId: {
+      type: mongoose.Schema.ObjectId,
+      ref: "product",
+    },
+    product_details: {
+      name: String,
+      image: Array,
+    },
+    payment_id: {
+      type: String,
+      default: "",
+    },
+    payment_status: {
+      type: String,
+      default: "",
+    },
+    delivery_address: {
+      type: mongoose.Schema.ObjectId,
+      ref: "address",
+    },
+    subTotalAmount: {
+      type: Number,
+      default: 0,
+    },
+    totalAmount: {
+      type: Number,
+      default: 0,
+    },
+    invoice_receipt: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+const OrderModel = mongoose.model("order", orderSchema);
+
+export default OrderModel;
---
+++ server/models/product.model.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema(
+  {
+    product_name: {
+      type: String,
+    },
+    image: {
+      type: Array,
+      default: [],
+    },
+    category: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "category",
+      },
+    ],
+    subCategory: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "subCategory",
+      },
+    ],
+    unit: {
+      type: String,
+      default: "",
+    },
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    discount: {
+      type: Number,
+      default: null,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    more_details: {
+      type: Object,
+      default: {},
+    },
+    publish: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ProductModel = mongoose.model("product", productSchema);
+
+export default ProductModel;
